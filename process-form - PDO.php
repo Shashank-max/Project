90b1105e6801080b06d4e5e1b3106dc8ec81fa96
Shashank-max <65381727+Shashank-max@users.noreply.github.com>
2023-01-21 16:55:52 +0530

    Add files via upload

---
+++ process-form - PDO.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$servername = "localhost";
+$dbname = "Information";
+$username = "root";
+$password = "";
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=Information",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+
+    $name = $_POST["name"];
+    $country_code = $_POST["country_code"];
+    $number = filter_input(INPUT_POST, "number", FILTER_VALIDATE_INT);
+    $email = $_POST["email"];
+    $location = $_POST["location"];
+
+    $stmt = $conn->prepare("INSERT INTO info (name, country_code, number, email, location)
+                            VALUES (:name, :country_code, :number, :email, :location)");
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':country_code', $country_code);
+    $stmt->bindParam(':number', $number);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':location', $location);
+
+
+    $stmt->execute();
+    echo "Record Saved";
+} catch (PDOException $e) {
+    echo "Error:" . $e->getMessage();
+}
+
+
+
+
+    
